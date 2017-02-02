a6956b03b7129e245b7830d92bb48e5bd0482782
Dm47021 <dm47021@hotmail.com>
2017-02-02 18:41:32 -0500

    add colors

--- pkg-brew/pkg-brew.sh
+++ pkg-brew/pkg-brew.sh
@@ -2,6 +2,9 @@
 
 # Source Based Package manager for LFS "Linux From Scratch" Based Systems
 # By Derek Midkiff e.g. Dm47021 (C) 2017 
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
 
 export HOST=lfs
 export BUILD_DIR=/usr/bpkg/source/$PACKAGE_NAME/BUILD
