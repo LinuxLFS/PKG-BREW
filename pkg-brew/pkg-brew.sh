ef077a57b52a98f38f6097b1ad6786e08a782e62
Dm47021 <dm47021@hotmail.com>
2017-02-02 18:37:55 -0500

    refactor to bpgk to keep from conflicting with the linux Brew Package

--- pkg-brew/pkg-brew.sh
+++ pkg-brew/pkg-brew.sh
@@ -4,14 +4,14 @@
 # By Derek Midkiff e.g. Dm47021 (C) 2017 
 
 export HOST=lfs
-export BUILD_DIR=/usr/brew/source/$PACKAGE_NAME/BUILD
+export BUILD_DIR=/usr/bpkg/source/$PACKAGE_NAME/BUILD
 
 # Functions
 
 function resolve-cpn-dir {     # gets Current Package Name Directory
 
 function get-source-pkg {
-            cd $BUILD_DIR  # cd into /usr/brew/source/$PACKAGE_NAME/BUILD
+            cd $BUILD_DIR  # cd into /usr/bpkg/source/$PACKAGE_NAME/BUILD
             rm -rf *
 
 
