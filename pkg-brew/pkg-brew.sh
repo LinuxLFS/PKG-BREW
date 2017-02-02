0d10a27ca51196f25b95a2ffbef70219138369aa
Dm47021 <dm47021@hotmail.com>
2017-02-02 18:34:53 -0500

    Initial repo setup

---
+++ pkg-brew/pkg-brew.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Source Based Package manager for LFS "Linux From Scratch" Based Systems
+# By Derek Midkiff e.g. Dm47021 (C) 2017 
+
+export HOST=lfs
+export BUILD_DIR=/usr/brew/source/$PACKAGE_NAME/BUILD
+
+# Functions
+
+function resolve-cpn-dir {     # gets Current Package Name Directory
+
+function get-source-pkg {
+            cd $BUILD_DIR  # cd into /usr/brew/source/$PACKAGE_NAME/BUILD
+            rm -rf *
+
+
